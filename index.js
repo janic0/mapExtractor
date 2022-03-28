35b5abea6b4ade446c0bce87bdc62399bd25163b
janic0 <git@janic.io>
2022-03-28 09:50:05 +0200

    fixed axios erros

--- index.js
+++ index.js
@@ -9,44 +9,50 @@ const fs = require("fs");
 const run = (url) => {
 	const fileName = url.split("/").pop();
 	if (!fileName) return console.log("Invalid URL");
-	axios.default.get(url).then((r) => {
-		if (
-			!Array.isArray(r.data.sources) ||
-			!Array.isArray(r.data.sourcesContent)
-		) {
-			console.error("Invalid response");
-			process.exit();
-		}
-		let completed = 0;
-		r.data.sources.forEach((source, i) =>
-			fs.mkdir(
-				join(__dirname, fileName, dirname(source)),
-				{ recursive: true },
-				(err) => {
-					if (err) {
-						completed++;
-						if (completed === r.data.sources.length) {
-							console.log("Done");
-							process.exit();
-						}
-						console.log(err);
-					} else
-						fs.writeFile(
-							join(__dirname, fileName, source),
-							r.data.sourcesContent[i],
-							(err) => {
-								if (err) console.log(err);
-								completed++;
-								if (completed === r.data.sources.length) {
-									console.log("Done");
-									process.exit();
-								}
+	axios.default
+		.get(url)
+		.then((r) => {
+			if (
+				!Array.isArray(r.data.sources) ||
+				!Array.isArray(r.data.sourcesContent)
+			) {
+				console.error("Invalid response");
+				process.exit();
+			}
+			let completed = 0;
+			r.data.sources.forEach((source, i) =>
+				fs.mkdir(
+					join(__dirname, fileName, dirname(source)),
+					{ recursive: true },
+					(err) => {
+						if (err) {
+							completed++;
+							if (completed === r.data.sources.length) {
+								console.log("Done");
+								process.exit();
 							}
-						);
-				}
-			)
-		);
-	});
+							console.log(err);
+						} else
+							fs.writeFile(
+								join(__dirname, fileName, source),
+								r.data.sourcesContent[i],
+								(err) => {
+									if (err) console.log(err);
+									completed++;
+									if (completed === r.data.sources.length) {
+										console.log("Done");
+										process.exit();
+									}
+								}
+							);
+					}
+				)
+			);
+		})
+		.catch(() => {
+			console.log("Failed to connect");
+			process.exit();
+		});
 };
 
 if (process.argv.length < 3) {
